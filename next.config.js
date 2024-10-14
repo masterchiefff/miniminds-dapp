2617f89d2286cbe1016eaff44a5444274191ee44
anwar254 <sadatt254@gmail.com>
2024-10-14 15:58:45 +0300

    fixing esm error

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    webpack: (config) => {
+        config.module.rules.push({
+          test: /\.m?js$/,
+          resolve: {
+            fullySpecified: false, 
+          },
+        });
+    
+        return config;
+    },
+}
 
 module.exports = nextConfig
